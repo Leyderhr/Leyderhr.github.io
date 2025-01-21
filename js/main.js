a406f423b4034c2e4981bc3b271e6b24b47069e7
Leyder Herrera Bello <165097459+Leyderhr@users.noreply.github.com>
2025-01-21 00:51:33 -0500

    Update main.js
    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById('download-cv').addEventListener('click', function(){
     const link = document.createElement('a');
     link.href = 'assets/cv/Curriculum Vitae.pdf';
-    link.download = 'LeyderCV';
+    link.download = 'LeyderCV.pdf';
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
